374bff0703d42df220ef7007b16364fdd11f97ce
vifr <viktor.fres@exxeta.com>
2022-12-04 20:10:35 +0100

    accomplished second day second start

--- strategyGuide/__init__.py
+++ strategyGuide/__init__.py
@@ -12,16 +12,17 @@ def readFromFile(textfile):
 # A/X Rock
 # B/Y Paper
 # C/Z Scissor
+# second start: the second column means, how the round shall end:
+# x lose
+# y draw
+# z win
 def winLossOrDraw(column1, column2):
     if column1 == 'A':
         if column2 == 'X':
-            #    return 'DRAW'
             return 3
         elif column2 == 'Y':
-            # return 'WIN'
             return 6
         else:
-            # return 'LOSS'
             return 0
     if column1 == 'B':
         if column2 == 'X':
@@ -49,11 +50,9 @@ def tournament(text_file):
     column = readFromFile(text_file)
     points = 0
     for x in iter(column):
-        points = points + shapePoints(x.strip()[2]) + winLossOrDraw(x.strip()[0], x.strip()[2])
-        print(points)
-
+        shape = getShape(x.strip()[0], x.strip()[2])
+        points = points + shapePoints(shape) + winLossOrDraw(x.strip()[0], shape)
     print(points)
-
 
 def shapePoints(column2):
     if column2 == 'X':
@@ -62,3 +61,29 @@ def shapePoints(column2):
         return 2
     elif column2 == 'Z':
         return 3
+
+def getShape(column1,column2):
+    # DRAw
+    if column2 == 'Y':
+        if column1 == 'A':
+            return 'X'
+        elif column1 == 'B':
+            return 'Y'
+        else:
+            return 'Z'
+    # LOSS
+    if column2 == 'X':
+        if column1 == 'A':
+            return 'Z'
+        elif column1 == 'B':
+            return 'X'
+        else:
+            return 'Y'
+    # WIN
+    if column2 == 'Z':
+        if column1 == 'A':
+            return 'Y'
+        elif column1 == 'B':
+            return 'Z'
+        else:
+            return 'X'
