5588e7c81b4b76ea13691c64457455ef05a2970f
vifr <viktor.fres@exxeta.com>
2022-12-05 14:27:05 +0100

    accomplished four day second star

--- Four/__init__.py
+++ Four/__init__.py
@@ -1,10 +1,8 @@
 import fileReader
 
-
 def campCleanup(textfile):
     column = fileReader.fileReader(textfile)
     compareAssignement(column)
-
 
 def compareAssignement(column):
     result = []
@@ -12,8 +10,16 @@ def compareAssignement(column):
         assign = line.split(',')
         assigneOne = assign[0].split('-')
         assigneTwo = assign[1].split('-')
-        if int(assigneOne[0]) >= int(assigneTwo[0]) and int(assigneOne[1]) <= int(assigneTwo[1]):
+        # second star
+        if overlaps(assigneOne, assigneTwo):
             result.append(line)
-        elif int(assigneTwo[0]) >= int(assigneOne[0]) and int(assigneTwo[1]) <= int(assigneOne[1]):
-            result.append(line)
+
+        ## first star
+        # if int(assigneOne[0]) >= int(assigneTwo[0]) and int(assigneOne[1]) <= int(assigneTwo[1]):
+        #    result.append(line)
+        # elif int(assigneTwo[0]) >= int(assigneOne[0]) and int(assigneTwo[1]) <= int(assigneOne[1]):
+        #    result.append(line)
     print(len(result))
+
+def overlaps(column1, column2):
+    return max(int(column1[0]), int(column2[0])) <= min(int(column1[1]), int(column2[1]))
