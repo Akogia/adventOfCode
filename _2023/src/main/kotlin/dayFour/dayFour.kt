886897fe376df9b8f84a80dcce4dd77be542b0e2
vifr <viktor.fres@exxeta.com>
2023-12-06 14:54:29 +0100

    second star, day four

--- _2023/src/main/kotlin/dayFour/dayFour.kt
+++ _2023/src/main/kotlin/dayFour/dayFour.kt
@@ -2,31 +2,41 @@ package dayFour
 
 import java.io.File
 
+val copiedElement = mutableListOf<Pair<Int,List<String>>>()
+var splitted = mutableListOf<List<String>>()
+data class Card(val cardID: Int, val winningNumber: List<Int>, val chosenNumber: List<Int>, var scratched: Boolean = false)
+
 fun main() {
     val fileContent = File("_2023/src/main/resources/dayFour.txt").readText(Charsets.UTF_8).split("\n")
-    val splitted = fileContent.map { it.split(":") }.map { it[1].split("|") }
-    println("splitted: $splitted")
-    var sum = 0
-    for (element in splitted) {
-        val numbers = element.map { it.split(" ").filter { it.isNotEmpty() }.map { it.toInt() } }
-        val winningNumbers = numbers[0]
-        val chosenNumbers = numbers[1]
-        println("winningNumbers: $winningNumbers, chosenNumbers: $chosenNumbers")
-        val commonNumbers = winningNumbers.intersect(chosenNumbers)
-        println("commonNumbers: $commonNumbers")
-        println("commonNumbers.size: ${commonNumbers.size}")
-        sum += doubleValue(commonNumbers.size)
-        println("sum: $sum")
-    }
+    val cards = fileContent.map { line ->
+        val parts = line.split(": ", " | ")
+        val cardID = parts[0].split("\\s+".toRegex())[1].toInt()
+        val winningNumber = parts[1].trim().split("\\s+".toRegex()).map { it.toInt() }
+        val chosenNumber = parts[2].trim().split("\\s+".toRegex()).map { it.toInt() }
+        Card(cardID, winningNumber, chosenNumber)
+    }.associateBy { it.cardID }.toMutableMap()
+    scratchCards(cards)
+    println(cards.size)
 }
 
-fun doubleValue(number: Int): Int {
-    if(number == 0) {
-        return 0
+fun scratchCards(cards: MutableMap<Int, Card>) {
+    var cardID = 1
+
+    while (cardID <= cards.size) {
+        val card = cards[cardID]
+        println("card: $card")
+        if (card != null && !card.scratched) {
+            val commonNumbers = card.winningNumber.intersect(card.chosenNumber)
+            println("commonNumbers: $commonNumbers")
+            for (i in 1..commonNumbers.size) {
+
+                val copy = cards[card.cardID + i]!!.copy()
+                copy.scratched = false
+                cards[cards.size + 1] = copy
+                println("copy: $copy")
+            }
+            card.scratched = true
+        }
+        cardID++
     }
-    var value = 1
-    for (i in 1..<number) {
-        value *= 2
-    }
-    return value
 }
